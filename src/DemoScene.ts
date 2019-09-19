6007992b8d8a3969b259f370fdf36f327de72b51
Jake Klassen <jklassendev@gmail.com>
2019-09-19 17:16:38 -0400

    new ship asset and getting rotation working with the right stick

--- src/DemoScene.ts
+++ src/DemoScene.ts
@@ -1,20 +1,37 @@
 import Phaser from 'phaser';
-import blueShip from '../assets/blue_ship.png';
+import shipUrl from '../assets/ship.png';
 
 export class DemoScene extends Phaser.Scene {
   gamepad: Phaser.Input.Gamepad.Gamepad | null = null;
-  player: Phaser.Physics.Arcade.Image | null = null;
+  player = {} as Phaser.Physics.Arcade.Image;
+
+  leftStick = {
+    x: {} as Phaser.Input.Gamepad.Axis,
+    y: {} as Phaser.Input.Gamepad.Axis,
+  };
+
+  rightStick = {
+    x: {} as Phaser.Input.Gamepad.Axis,
+    y: {} as Phaser.Input.Gamepad.Axis,
+  };
+
+  // @ts-ignore
+  inputText: Phaser.GameObjects.Text;
 
   preload() {
-    this.load.image('blue_ship', blueShip);
+    this.load.image('ship', shipUrl);
   }
 
   create() {
+    this.inputText = this.add.text(10, 10, [], {
+      fill: '#ffffff',
+    });
+
     // Create the player
     this.player = this.physics.add.image(
       +this.game.config.width / 2,
       +this.game.config.height / 2,
-      'blue_ship',
+      'ship',
     );
     this.player.setDamping(true);
     this.player.setDrag(0.95);
@@ -22,8 +39,9 @@ export class DemoScene extends Phaser.Scene {
     this.player.setCollideWorldBounds(true);
 
     // Seems to require button press to fire this off
-    this.input.gamepad.once('connected', () => {
+    this.input.gamepad.on('connected', () => {
       console.log('connected');
+      console.log(this.input.gamepad.gamepads[0].id);
     });
 
     this.input.gamepad.on(
@@ -36,8 +54,17 @@ export class DemoScene extends Phaser.Scene {
         console.log({ pad, button, index });
 
         if (this.gamepad == null) {
-          pad.setAxisThreshold(0.3);
+          pad.setAxisThreshold(0.5);
+
           this.gamepad = pad;
+          this.leftStick = {
+            x: pad.axes[0],
+            y: pad.axes[1],
+          };
+          this.rightStick = {
+            x: pad.axes[2],
+            y: pad.axes[3],
+          };
         }
       },
       this,
@@ -47,13 +74,19 @@ export class DemoScene extends Phaser.Scene {
   update() {
     if (!this.gamepad) return;
 
-    this.player!.setAngularVelocity(300 * this.gamepad!.leftStick.x);
+    this.inputText.setText([
+      `left: ${this.leftStick.x.value}, ${this.leftStick.y.value}`,
+      `right: ${this.rightStick.x.value}, ${this.rightStick.y.value}`,
+      `axes: ${this.gamepad.axes.length}`,
+    ]);
+
+    this.player.setAngularVelocity(200 * this.gamepad!.rightStick.x);
 
     if (this.gamepad.leftStick.y <= 0) {
       this.physics.velocityFromRotation(
-        this.player!.rotation,
-        Math.abs(800 * this.gamepad.leftStick.y),
-        (this.player!.body as Phaser.Physics.Arcade.Body).acceleration,
+        this.player.rotation,
+        Math.abs(900 * this.gamepad.leftStick.y),
+        (this.player.body as Phaser.Physics.Arcade.Body).acceleration,
       );
     }
   }
--- src/game.ts
+++ src/game.ts
@@ -4,8 +4,8 @@ import { DemoScene } from './DemoScene';
 const config: Phaser.Types.Core.GameConfig = {
   title: 'Spacefighters',
   type: Phaser.AUTO, // Uses WebGL with Canvas2DRenderingContext fallback
-  width: 800,
-  height: 600,
+  width: window.innerWidth,
+  height: window.innerHeight,
   scene: DemoScene,
   parent: '#container', // Where to inject the canvas element
   fps: {
